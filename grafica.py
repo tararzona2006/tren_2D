349c42cb71e19fb204b89aab86b1007e6d05ecac
sharol2021 <sharollopez@colegioguanenta.edu.co>
2023-05-26 06:56:02 -0500

    se modifico

--- grafica.py
+++ grafica.py
@@ -26,6 +26,7 @@ c = Canvas(frame_graficacion, width=BASE,height=ALTURA)
 c.config(bg="white")
 c.place(x=10,y=10)
 # dibujar rectangulos
+circ_1 = c.create_oval(BASE/2-185,ALTURA/2+10,BASE/2-110,ALTURA-100, fill ="black")
 rect_1 = c. create_rectangle(BASE/2-115,ALTURA-75,BASE-30,ALTURA/2,fill="light grey")
 
 
@@ -46,5 +47,11 @@ rect_8 = c. create_rectangle(BASE/2+30,ALTURA/2-140,BASE-70,ALTURA/2-170, fill="
 rect_9 = c. create_rectangle(BASE/2+20,ALTURA-220,BASE-65,ALTURA/2-95,fill="light grey")
 rect_10 = c. create_rectangle(BASE/2-135,ALTURA-100,BASE/2-115,ALTURA/2+15,fill="dark gray")
 rect_10 = c. create_rectangle(BASE/2-155,ALTURA-75,BASE/2-130,ALTURA/2,fill="dark gray")
+circ_4 = c.create_oval(BASE/2+50,ALTURA/2-20,BASE/2+130,ALTURA-295, fill="yellow")
+circ_5 = c.create_oval(BASE/2+100,ALTURA/2-60,BASE/2+120,ALTURA-280, fill="white")
+circ_5 = c.create_oval(BASE/2+60,ALTURA/2-60,BASE/2+80,ALTURA-280, fill="white")
+linea_1 = c. create_line(BASE/2+80,ALTURA/2-40,BASE/2+100,ALTURA-240, fill="black",width=2)
+nombre_1 = c. create_text(BASE/2,ALTURA/1.8,anchor="center",text="tarazona",font=("arial",25, "bold"),fill="blue",activefill="yellow")
+
 # desplegar ventana
 ventana_principal.mainloop()
